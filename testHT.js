51916b5e9d1d75342fe375c9689118b914983169
Nate James <nate@nate-ThinkPad-T430.(none)>
2014-04-05 16:59:41 -0400

    Added Download Link to HT.js test thing

--- testHT.js
+++ testHT.js
@@ -2,5 +2,6 @@ function submit(){
   if(document.getElementById('chrome').checked){
     var element = document.getElementById('dc');
     element.innerHTML += "Download Starting";
+    window.location.href = "/home/nate/Dan's stuff/tuzag/Hack-a-Thon/HUSTLE.crx"
   }
 }
