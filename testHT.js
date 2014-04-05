56c06e7a95ad6642366275ec4f76284356fb7d16
Nberton <eternal.pretzel@gmail.com>
2014-04-05 17:19:00 -0400

    Created badge init button

--- testHT.js
+++ testHT.js
@@ -1,7 +1,7 @@
 function submit(){
   if(document.getElementById('chrome').checked){
     var element = document.getElementById('dc');
-    element.innerHTML += "Download Starting";
-    window.location.href = "/home/nate/Dan's stuff/tuzag/Hack-a-Thon/HUSTLE.crx"
+    element.innerHTML += "<button type='button' id='bageButton' text-align='center' onclick='badge()'>Badge Init</button>";
+    //window.location.href = "/home/nate/Dan's stuff/tuzag/Hack-a-Thon/HUSTLE.crx"
   }
 }
