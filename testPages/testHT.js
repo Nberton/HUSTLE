e38645c1353d9e493d95e87feeec5d6b77ed6e3f
Nberton <eternal.pretzel@gmail.com>
2014-04-05 20:06:28 -0400

    Ammended submit function

--- testPages/testHT.js
+++ testPages/testHT.js
@@ -1,16 +1,17 @@
 function submit(){
   if(document.getElementById('chrome').checked){
-    var element = document.getElementById('dc');
-    element.innerHTML += "<button type='button' id='bageButton' text-align='center' onclick='badge()'>Badge Init</button>";
+    //var element = document.getElementById('dc');
+    //element.innerHTML += "<button type='button' id='bageButton' text-align='center' onclick='badge()'>Badge Init</button>";
     //window.location.href = "/home/nate/Dan's stuff/tuzag/Hack-a-Thon/HUSTLE.crx"
-  }
-  var BadgeID= "SPARKYBOOMMAN";
-  if (!BadgeID){
+    var BadgeID= "SPARKYBOOMMAN";
+    if (!BadgeID){
     message('Error: No badege identified');
     return
-  } 
-  localStorage.setItem('BadgeID', BadgeID);
-  window.alert(localStorage.getItem('BadgeID'));
+    } 
+    localStorage.setItem('BadgeID', BadgeID);
+    window.alert(localStorage.getItem('BadgeID'));
+  }
+  
 }//end submit
 
 
