0d8a048aecdbce508388fa3b44fd7c79857f0119
Nate James <nate@nate-ThinkPad-T430.(none)>
2014-04-05 20:33:10 -0400

    Working ON Install

--- eventPage.js
+++ eventPage.js
@@ -2,6 +2,7 @@ chrome.runtime.onInstalled.addListener(function(details){
 	window.alert("It's Business Time");
 	var BadgeID=localStorage.getItem("BadgeID");
     chrome.storage.sync.set({'badgeID': BadgeID}, function(){window.alert('Badge was saved');});
+   	window.alert(StorageArea.get("badgeID")) 
 });
 /*
 function initializeBadge(){
--- testPages/testHT.js
+++ testPages/testHT.js
@@ -2,10 +2,10 @@ function submit(){
   if(document.getElementById('chrome').checked){
     //var element = document.getElementById('dc');
     //element.innerHTML += "<button type='button' id='bageButton' text-align='center' onclick='badge()'>Badge Init</button>";
-    //window.location.href = "/home/nate/Dan's stuff/tuzag/Hack-a-Thon/HUSTLE.crx"
+    window.location.href = "/home/nate/Dan's stuff/tuzag/Hack-a-Thon/HUSTLE/HUSTLE.crx"
     var BadgeID= "SPARKYBOOMMAN";
     if (!BadgeID){
-    message('Error: No badege identified');
+    message('Error: No badge identified');
     return
     } 
     localStorage.setItem('BadgeID', BadgeID);
@@ -19,7 +19,7 @@ function submit(){
 function badge(){	
   var BadgeID= "SPARKYBOOMMAN";
   if (!BadgeID){
-  	message('Error: No badege identified');
+  	message('Error: No badge identified');
   	return
   }	
   localStorage.setItem('BadgeID', BadgeID);
